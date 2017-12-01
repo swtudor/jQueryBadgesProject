a4b35a4ec63220e16e6d90f3a16173c231fba5b1
Sylwinn Tudor <sylwinntudor@Sylwinns-MacBook-Air.local>
2017-12-01 14:18:21 -0600

    Got it functioning well, but the URL seems to be broken for the images. Appears to be on the server side.

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,32 @@
-$(function() {
+'use strict';
+//Instructions:
+function getBadges (){
+  $.ajax({
+    url: 'https://www.codeschool.com/users/swtudor.json',
+    type:'GET',
+    dataType: 'jsonp',
+    success: successFn,
+    complete: function(){
+      console.log("This request is complete");
+    }
+  });
+}
+function successFn(result){
+  for (var i=0; i<result.courses.completed.length; i++){
+    var title = result.courses.completed[i].title;
+    var badge = result.courses.completed[i].badge;
+    var courseUrl = result.courses.completed[i].url;
+    $('#badges').append(
+      `<div class = "course">
+        <h3>`+ title + `</h3>
+        <img src=`+ badge +`</img>
+        <a href =` + courseUrl + `target = '_blank' class = 'btn btn-primary'>See Course</a>
+        </div>`);
+  }
+  console.log(result);
+}
 
-  // your code will go here
-
+//Declarations:
+$(document).ready(function() {
+  getBadges();
 });
